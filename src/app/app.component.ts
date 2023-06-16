1e9a82cda510fb2061620feff669ab56886fe03c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-16 14:55:57 -0400

    Add project title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'proj1';
+  title = 'Angular Proj1';
 }
