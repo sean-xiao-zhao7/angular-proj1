fd43d1d4646fd94e198ca9878c6de334405cc290
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-19 15:30:26 -0400

    Add ServerComponent to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './components/server/server.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
