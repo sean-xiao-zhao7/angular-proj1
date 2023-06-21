575a6d8397a72b0289657ac0766825a46d705efb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-21 14:20:25 -0400

    Change h1 color in server

--- src/app/components/server/server.component.ts
+++ src/app/components/server/server.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: ['h1 { color: red; }'],
 })
 export class ServerComponent {}
