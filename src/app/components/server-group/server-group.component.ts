e3c74e0ed1389c77553de68f3a0a52e6a8675e22
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-21 14:47:56 -0400

    Add onAddServer

--- src/app/components/server-group/server-group.component.ts
+++ src/app/components/server-group/server-group.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server-group',
   templateUrl: './server-group.component.html',
-  styleUrls: ['./server-group.component.css']
+  styleUrls: ['./server-group.component.css'],
 })
 export class ServerGroupComponent {
-
+  onAddServer() {}
 }
